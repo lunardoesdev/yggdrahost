51e498249b709c0bb05b868b22b07e8297161a74
Lunar <lunarlifeburst@gmail.com>
2026-03-10 14:42:57 +1000

    try 1: simple examplwe

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import {Elysia, t} from 'elysia'
+
+const apiExample = new Elysia()
+    .get("/", () => "api root")
+
+const app = new Elysia()
+    .onBeforeHandle((request) => {
+        const host = request.headers.host?.split(":")[0];
+        if (host == "api.example.com") {
+            return apiExample.handle(request.request)
+        }
+    })
